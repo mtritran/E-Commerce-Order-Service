3651e640d665c937e4726fe61507b148095b8c05
mtritran <tranmanhtri5104@gmail.com>
2025-10-03 16:30:30 +0700

    init spring boot project

---
+++ src/main/java/com/mtritran/e_commerce_order_service/ECommerceOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mtritran.e_commerce_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceOrderServiceApplication.class, args);
+	}
+
+}

70842617dc375b3f0c6850bbbbe2c77b58f5579c
mtritran <tranmanhtri5104@gmail.com>
2025-10-03 22:51:34 +0700

    feat: add validate to user creation and update dto

--- src/main/java/com/mtritran/e_commerce_order_service/dto/request/UserCreationRequest.java
+++ src/main/java/com/mtritran/e_commerce_order_service/dto/request/UserCreationRequest.java
@@ -1,5 +1,7 @@
 package com.mtritran.e_commerce_order_service.dto.request;
 
+import com.mtritran.e_commerce_order_service.validator.DobConstraint;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -11,9 +13,13 @@ import java.time.LocalDate;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserCreationRequest {
+    @Size(min = 3, message = "USER_NAME_INVALID")
     String username;
+    @Size(min = 7, message = "PASSWORD_INVALID")
     String password;
     String firstname;
     String lastname;
+    @DobConstraint(min = 18, message = "DOB_INVALID")
     LocalDate dob;
 }
+

40b5ddd7790eef4df0ac49f41659ec3d6012b14a
mtritran <tranmanhtri5104@gmail.com>
2025-10-03 22:49:28 +0700

    feat: add validator for dob

---
+++ src/main/java/com/mtritran/e_commerce_order_service/validator/DobValidator.java
@@ -0,0 +1,26 @@
+package com.mtritran.e_commerce_order_service.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
+public class DobValidator implements ConstraintValidator<DobConstraint, LocalDate> {
+
+    private int min;
+
+    @Override
+    public void initialize(DobConstraint constraintAnnotation) {
+        min = constraintAnnotation.min();
+    }
+
+    @Override
+    public boolean isValid(LocalDate value, ConstraintValidatorContext context) {
+        if (Objects.isNull(value)) return true; // cho phép dob null
+
+        long years = ChronoUnit.YEARS.between(value, LocalDate.now());
+        return years >= min;
+    }
+}

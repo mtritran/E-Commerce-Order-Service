2f315570afcafe192e21623be49ab829fc848ca7
mtritran <tranmanhtri5104@gmail.com>
2025-10-03 22:47:50 +0700

    feat: add function to permission, role, user repo

--- src/main/java/com/mtritran/e_commerce_order_service/repository/PermissionRepository.java
+++ src/main/java/com/mtritran/e_commerce_order_service/repository/PermissionRepository.java
@@ -4,6 +4,14 @@ import com.mtritran.e_commerce_order_service.entity.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
 @Repository
 public interface PermissionRepository extends JpaRepository<Permission, String> {
+    void deleteByName(String name);
+    Optional<Permission> findByName(String name);
+    boolean existsByName(String name);
+    List<Permission> findByNameIn(Set<String> names);
 }
--- src/main/java/com/mtritran/e_commerce_order_service/repository/RoleRepository.java
+++ src/main/java/com/mtritran/e_commerce_order_service/repository/RoleRepository.java
@@ -4,6 +4,14 @@ import com.mtritran.e_commerce_order_service.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role, String> {
+    Optional<Role> findByName(String name);
+    void deleteByName(String name);
+    boolean existsByName(String name);
+    List<Role> findByNameIn(Set<String> names);
 }

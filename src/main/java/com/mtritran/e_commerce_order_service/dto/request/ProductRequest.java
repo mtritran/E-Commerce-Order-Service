d940da5699338855a304b46e1026c2182b6cedf9
mtritran <tranmanhtri5104@gmail.com>
2025-10-04 15:10:37 +0700

    feat: add product request

---
+++ src/main/java/com/mtritran/e_commerce_order_service/dto/request/ProductRequest.java
@@ -0,0 +1,16 @@
+package com.mtritran.e_commerce_order_service.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ProductRequest {
+    String name;
+    String description;
+    Double price;
+    Integer stock;
+}

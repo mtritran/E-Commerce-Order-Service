80cbb415783987117a2f262e9f6b0de5764b43bd
mtritran <tranmanhtri5104@gmail.com>
2025-10-06 12:23:05 +0700

    feat: add delete expiry time token function

--- src/main/java/com/mtritran/e_commerce_order_service/repository/InvalidatedTokenRepository.java
+++ src/main/java/com/mtritran/e_commerce_order_service/repository/InvalidatedTokenRepository.java
@@ -4,6 +4,9 @@ import com.mtritran.e_commerce_order_service.entity.InvalidatedToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface InvalidatedTokenRepository extends JpaRepository<InvalidatedToken, String> {
+    int deleteByExpiryTimeBefore(LocalDateTime time);
 }
